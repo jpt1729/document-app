d2040ecb0ecc23887b3c7c8ae7007638c9a3b0ce
John <136768731+jpt1729@users.noreply.github.com>
2024-01-24 23:03:05 -0600

    env fixed

--- src/app/api/auth/[...nextauth].js
+++ src/app/api/auth/[...nextauth].js
@@ -3,6 +3,14 @@ import GoogleProvider from 'next-auth/providers/google'
 
 export const authOptions = {
   // Configure one or more authentication providers
+  callbacks: {
+    async signIn({ account, profile }) {
+      if (account.provider === "google") {
+        return profile.email_verified && profile.email.endsWith("@example.com")
+      }
+      return true // Do different verification for other providers that don't have `email_verified`
+    },
+  },
   providers: [
     GoogleProvider({
         clientId: process.env.GOOGLE_CLIENT_ID,
